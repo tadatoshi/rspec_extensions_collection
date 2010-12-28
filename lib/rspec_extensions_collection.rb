38341741da0922ec81054a9d06038b12e3017397
Tadatoshi Takahashi <tadatoshi@Tadatoshi-Takahashis-MacBook.local>
2010-12-28 13:55:52 -0500

    Required search_embedded_matcher in lib/rspec_extensions_collection.rb.

--- lib/rspec_extensions_collection.rb
+++ lib/rspec_extensions_collection.rb
@@ -1,5 +1,3 @@
-# The following doesn't work because when this file is loaded by another project, the current directory is in that project:
-# Dir[File.join("rspec_extensions_collection/**/*.rb")].each {|f| require f}
-
 require 'rspec_extensions_collection/helpers/mongoid_model_helpers'
 require 'rspec_extensions_collection/matchers/search_matcher'
+require 'rspec_extensions_collection/matchers/search_embedded_matcher'
