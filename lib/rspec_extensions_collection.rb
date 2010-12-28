d3f475c07c22f7b58dc92a73d077b97885c2e72c
Tadatoshi Takahashi <tadatoshi@Tadatoshi-Takahashis-MacBook.local>
2010-12-27 20:25:20 -0500

    In the file that is loaded when gem is loaded, the required files are specified individually.

--- lib/rspec_extensions_collection.rb
+++ lib/rspec_extensions_collection.rb
@@ -1,1 +1,5 @@
-Dir[File.join("rspec_extensions_collection/**/*.rb")].each {|f| require f}
+# The following doesn't work because when this file is loaded by another project, the current directory is in that project:
+# Dir[File.join("rspec_extensions_collection/**/*.rb")].each {|f| require f}
+
+require 'rspec_extensions_collection/helpers/mongoid_model_helpers'
+require 'rspec_extensions_collection/matchers/search_matcher'
